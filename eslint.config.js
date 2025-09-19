e7f1b14eb8be25aeae74aa5f3230e8f973ad12e1
Mark Norgren <mark@marknorgren.com>
2025-09-18 20:25:12 -0500

    chore: configure linting and formatting tooling

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,27 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const prettierConfig = require('eslint-config-prettier');
 
 module.exports = defineConfig([
-  expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/**', 'build/**', 'artifacts/**', 'eslint.config.js'],
   },
+  ...expoConfig,
+  {
+    files: ['scripts/**/*.{js,mjs,ts}'],
+    rules: {
+      'expo/no-dynamic-env-var': 'off',
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+  prettierConfig,
 ]);
